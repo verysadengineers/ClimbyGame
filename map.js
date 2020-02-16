35a796e992c0c306a9a2141fced1ca3af4fad8ac
alvindav <alvind@sfu.ca>
2020-02-16 12:43:48 -0800

    two map levels

--- map.js
+++ map.js
@@ -1,103 +1,98 @@
 export function generateMap(gameScene, player){
 
   let tiles = gameScene.physics.add.staticGroup();
-  let tileSize = 30;
+  let paths = gameScene.physics.add.staticGroup();
+
+
+  let tileDistance = 26;
+
+
   let middleWidth = 400;
-  var x=12;
-  var y=10;
+  var x=26;
+  var y=20;
   var n=x*y-1;
-  	if (n<0) {alert("illegal maze dimensions");return;}
-  	var horiz =[];
-    var verti =[];
-    for (j= 0; j<x+1; j++){
-      horiz[j]= [];
-    }
-    for (j= 0; j<x+1; j++){
-      verti[j]= []
-    }
-  	var here = [Math.floor(Math.random()*x), Math.floor(Math.random()*y)];
-  	var path = [here];
-  	var unvisited = [];
-  	for (j = 0; j<x+2; j++){
-  		unvisited[j] = [];
-  	for (var k= 0; k<y+1; k++)
-  			unvisited[j].push(j>0 && j<x+1 && k>0 && (j != here[0]+1 || k != here[1]+1));
-  	}
-  	while (0<n) {
-  		var potential = [[here[0]+1, here[1]], [here[0],here[1]+1],
-  		    [here[0]-1, here[1]], [here[0],here[1]-1]];
-  		var neighbors = [];
-  		for (var j = 0; j < 4; j++){
-  			if (unvisited[potential[j][0]+1][potential[j][1]+1]){
-  				neighbors.push(potential[j]);
-        }
-      }
-  		if (neighbors.length) {
-  			n = n-1;
-  			var next = neighbors[Math.floor(Math.random()*neighbors.length)];
-  			unvisited[next[0]+1][next[1]+1]= false;
-  			if (next[0] == here[0]){
-  				horiz[next[0]][(next[1]+here[1]-1)/2]= true;
-        }
-  			else{
-  				verti[(next[0]+here[0]-1)/2][next[1]]= true;
-        }
-  			path.push(here = next);
-  		} else
-  			here = path.pop();
-  	}
 
-    for (j= 0; j<x*2+1; j++) {
-    		var line= [];
-    		if (0 == j%2)
-    			for (k=0; k<y*4+1; k++)
-    				if (0 == k%4)
-    					tiles.create(j*tileSize/2 + (player ? 0 : middleWidth), k*tileSize/2, 'tiles').setScale(0.5).setOrigin(0);
-    				else
-    					if (j>0 && verti[j/2-1][Math.floor(k/4)])
-    					     console.log("path")
-    					else
-    						tiles.create(j*tileSize/2 + (player ? 0 : middleWidth), k*tileSize/2, 'tiles').setScale(0.5).setOrigin(0);
-    		else
-    			for (k=0; k<y*4+1; k++)
-    				if (0 == k%4)
-    					if (k>0 && horiz[(j-1)/2][k/4-1])
-    						console.log("path");
-    					else
-    						tiles.create(j*tileSize/2 + (player ? 0 : middleWidth), k*tileSize/2, 'tiles').setScale(0.5).setOrigin(0);
-    				else
-    					console.log("path")
-    		if (0 == j)
-          console.log("path");
-    		if (x*2-1 == j)
-          console.log("path");
-        }
 
-  /*
-  let tileSize = 30;
-
-  let tiles = gameScene.physics.add.staticGroup();
-
-  var mazeArray = []; // Initialize array
-  for (var i = 0 ; i < 30; i++) {
-      mazeArray[i] = []; // Initialize inner array
-      for (var j = 0; j < 20; j++) {
-          mazeArray[i][j] = Math.round(Math.random()); //generate a 0 or 1
-      }
+  var mazeArray = [];
+  for(var index = 0; index < 25; index++){
+    mazeArray[index] = [];
   }
 
+  mazeArray[0] =  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[1] =  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[2] =  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[3] =  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[4] =  [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1];
+  mazeArray[5] =  [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[6] =  [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[7] =  [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[8] =  [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[9] =  [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[10] = [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[11] = [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1];
+  mazeArray[12] = [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[13] = [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[14] = [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[15] = [1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[16] = [1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[17] = [1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[18] = [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1];
+  mazeArray[19] = [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[20] = [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[21] = [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1];
+  mazeArray[22] = [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1];
+  mazeArray[23] = [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1];
+  mazeArray[24] = [1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1];
+  mazeArray[25] = [1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1];
+  mazeArray[26] = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1];
+  mazeArray[27] = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1];
+  mazeArray[28] = [1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1];
+  mazeArray[29] = [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1];
+  mazeArray[30] = [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1];
 
-  //display all tiles if value in array = 1
-  for (var i = 0 ; i < 30; i++) {
-      for (var j = 0; j < 20; j++) {
-        if(mazeArray[i][j] == 1){
-          tiles.create(i*tileSize, j*tileSize, 'tiles').setOrigin(0,0);
-        }
+  mazeArray[31] =  [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[32] =  [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[33] =  [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[34] =  [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[35] =  [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[36] =  [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1];
+  mazeArray[37] =  [1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1];
+  mazeArray[38] =  [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[39] =  [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[40] =  [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[41] = [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[42] = [1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[43] = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[44] = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[45] = [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[46] = [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[47] = [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[48] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[49] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1];
+  mazeArray[50] = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1];
+  mazeArray[51] = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1];
+  mazeArray[52] = [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[53] = [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[54] = [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[55] = [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[56] = [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[57] = [1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[58] = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[59] = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[60] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  mazeArray[61] = [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+
+  for(var i = 0; i < 32; i++){
+    for(var j = 0; j < 26; j++){
+      if(mazeArray[i][j] == 0){
+        paths.create(j*tileDistance, i*tileDistance, 'path').setOrigin(0).setScale(0.866);
       }
+      else {
+        tiles.create(j*tileDistance, i*tileDistance, 'wall').setOrigin(0).setScale(0.866);
+      }
+    }
   }
-  */
-
-
 
   return tiles;
 }
