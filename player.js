311209140bc654688dc6c5fe62e88c9f184928d8
a-arshad <ali.stick.arshad@gmail.com>
2020-02-16 17:02:26 -0800

    removed old sprite

--- player.js
+++ player.js
@@ -20,26 +20,6 @@ export function createPlayerTwo(game)
 export function initAnimations(game) 
 {
     game.anims.create({
-        key: 'left',
-        frames: game.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    game.anims.create({
-        key: 'turn',
-        frames: [ { key: 'dude', frame: 4 } ],
-        frameRate: 20
-    });
-
-    game.anims.create({
-        key: 'right',
-        frames: game.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    game.anims.create({
         key: 'blueMove',
         frames: game.anims.generateFrameNumbers('blue', { start: 0, end: 3 }),
         frameRate: 10,
